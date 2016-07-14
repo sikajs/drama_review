384d63efb9217c2fdd601f4957747e6885f3b07c
Jason Shen <sikajs@gmail.com>
2016-07-14 11:02:16 +0800

    Add authentication and account editing

--- app/controllers/dramas_controller.rb
+++ app/controllers/dramas_controller.rb
@@ -1,5 +1,6 @@
 class DramasController < ApplicationController
   before_action :find_drama, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @dramas = Drama.all.order("created_at DESC")
