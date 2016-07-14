e3a15ebd320ce595eccbeb7098ef369d3bfec1ac
Jason Shen <sikajs@gmail.com>
2016-07-14 17:29:10 +0800

    Add validations for drama

--- app/models/drama.rb
+++ app/models/drama.rb
@@ -5,4 +5,6 @@ class Drama < ActiveRecord::Base
   has_attached_file :image, styles: { medium: "500x700#", small: "300x500#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
+  validates :title, presence: true
+  validates :description, presence: true
 end
