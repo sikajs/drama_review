18b578b647ea9436e4153e2481ef731398aa0b24
Jason Shen <sikajs@gmail.com>
2016-07-14 12:05:26 +0800

    refactor drama show page and resize image

--- app/models/drama.rb
+++ app/models/drama.rb
@@ -1,7 +1,7 @@
 class Drama < ActiveRecord::Base
   belongs_to :user
 
-  has_attached_file :image, styles: { medium: "600x800#", small: "300x500#" }
+  has_attached_file :image, styles: { medium: "500x700#", small: "300x500#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 end
