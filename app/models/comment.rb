b229c80aff5da8ca2da937fe6579eace8bfa7b9a
Jason Shen <sikajs@gmail.com>
2016-07-16 20:21:08 +0800

    Add user profile and comment count, also adjust user_panel style

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ActiveRecord::Base
   scope :recent_first, -> { order("created_at DESC") }
 
   belongs_to :drama
-  belongs_to :user
+  belongs_to :user, counter_cache: true
 
   validates :content, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:show]
+
   resources :dramas do
     resources :comments, except: [:index, :show]
     collection do
