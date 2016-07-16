67f46d635d117030cf575fab38cbfb8d94de7c37
Jason Shen <sikajs@gmail.com>
2016-07-16 14:57:33 +0800

    Change ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-server 'ec2-52-197-193-53.ap-northeast-1.compute.amazonaws.com', user: 'deploy', roles: %w{web app db}
+server 'ec2-52-196-120-89.ap-northeast-1.compute.amazonaws.com', user: 'deploy', roles: %w{web app db}
 
 
 # role-based syntax
