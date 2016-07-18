bcdedb0a0bdbfa362017dda631e9dd5f412366a8
Jason Shen <sikajs@gmail.com>
2016-07-18 16:59:52 +0800

    Add drama controller test case

--- spec/controllers/dramas_controller_spec.rb
+++ spec/controllers/dramas_controller_spec.rb
@@ -1,34 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe DramasController, type: :controller do
-  let(:drama) { FactoryGirl.build(:drama) }
+  let(:user) { FactoryGirl.create(:user)}
 
-  before do
-    sign_in_as!
+  before :each do
+    sign_in_as!(user)
   end
 
   describe "#create" do
-    it "redirects to drama show page upon save" do
-      post :create, drama: drama
-      expect redirect_to drama
+    context "with valid attributes" do
+      it "redirects to drama show page upon save" do
+        @drama = FactoryGirl.build(:drama, user: user)
+        post :create, drama: @drama
+        expect redirect_to Drama.last
+      end
+    end
+
+    context "with invalid attributes" do
+      it "renders the :new tenplate" do
+        @drama = FactoryGirl.build(:invalid_drama, user: user)
+        post :create, drama: @drama
+        expect render_template :new
+      end
     end
   end
 
   describe "#update" do
     it "redirects to drama show page upon save" do
-      skip
+      @drama = FactoryGirl.create(:drama, user: user)
+      @drama.description = "Updated description"
+      put :update, id: @drama.id
+      expect redirect_to @drama
     end
   end
 
   describe "#destroy" do
     it "redirects to home page upon save" do
-      skip
+      @drama = FactoryGirl.create(:drama, user: user)
+      delete :destroy, id: @drama.id
+      expect redirect_to root_path
     end
   end
 
-  describe "#search" do
-    it "redirects to search result page upon save" do
-      skip
-    end
-  end
 end
--- spec/factories/drama.rb
+++ spec/factories/drama.rb
@@ -2,6 +2,11 @@ FactoryGirl.define do
   factory :drama do |f|
     f.title "test drama"
     f.description "test drama description"
-    f.actors "fake actor"
+    f.num_of_chapter 20
+    f.actors Faker::Name.name
+  end
+
+  factory :invalid_drama, parent: :drama do |f|
+    f.title nil
   end
 end
