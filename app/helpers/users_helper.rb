457c76c8042f58988c190b273222a3e45a9dcb27
Jason Shen <sikajs@gmail.com>
2016-07-18 09:18:34 +0800

    Add users_helper test and refactor users_helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
-  def show_user_comments_count
-    if @user.comments_count.nil?
+  def show_user_comments_count(user)
+    if user.comments_count.nil?
       0
     else
-      @user.comments_count
+      user.comments_count.to_i
     end
   end
 end
--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -11,5 +11,19 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe UsersHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "when call to show the number of user has commented" do
+    let(:drama) { Drama.create!(title: "test drama", description: "test drama description") }
+    let(:user) { User.create!(name: "user1", email: "user1@example.com", password: "test12", password_confirmation: "test12") }
+    let(:comment) { Comment.new(drama: drama, user: user) }
+
+    it "should be 0 if user never comment on drama" do
+      expect(show_user_comments_count(user)).to eq(0)
+    end
+
+    it "should be 1 if user commented only once for a drama" do
+      comment.content = "blah blah"
+      comment.save!
+      expect(show_user_comments_count(user)).to eq(1)
+    end
+  end
 end
