36e15384405884d96a86d7489683b0b5929ab17e
Jason Shen <sikajs@gmail.com>
2016-07-24 15:24:03 +0800

    Adjust rspec support helper for i18n

--- spec/support/devise_helper.rb
+++ spec/support/devise_helper.rb
@@ -1,6 +1,6 @@
 def sign_in_as!(user=FactoryGirl.build(:user))
-  visit new_user_session_path
+  visit new_user_session_path({locale: 'en'})
   fill_in "Email", with: user.email
   fill_in "Password", with: user.password
-  click_button "Log in"
+  click_button "Log In"
 end
