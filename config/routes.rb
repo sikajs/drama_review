152b8ee578e0161d35e1c79f59f42fd85f24349d
Jason Shen <sikajs@gmail.com>
2016-07-25 23:00:47 +0800

    change user profile path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-  resources :users, only: [:show]
+  resources :users, only: [:show], as: 'user_profile'
 
   get '/change_locale/:locale', to: 'users#change_locale', as: :change_locale
 
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
     collection do
       get 'search'
     end
+  end
+
+  namespace :admin do
+    resources :users
   end
 
   root "dramas#index"
